b20954a89800906ca1550566c50e8b75c1ed16c4
Unknown <sophie.candel@etu.unistra.fr>
2018-04-10 17:12:18 +0200

    MAJ

--- dev/angular/controllers/main.js
+++ dev/angular/controllers/main.js
@@ -1,23 +1,4 @@
-app.controller("main", [
-  "$scope",
-  function($scope) {
-    // $scope.openMenu = function() {
-    //   $("#menu").css("left", "0");
-    //   $("#open-menu").css("display", "none");
-    //   $("#close-menu").css("display", "block");
-    // };
-    // $scope.closeMenu = function() {
-    //   $("#menu").css("left", "-80%");
-    //   $("#open-menu").css("display", "block");
-    //   $("#close-menu").css("display", "none");
-    // };
-    // $scope.openMenu = function() {
-    //   $("#menu").css("left", "0");
-    //   $("#open-menu").hide;
-    // };
-    // $scope.openMenu();
-  }
-]);
+app.controller("main", ["$scope", function($scope) {}]);
 
 app.filter("trustUrl", [
   "$sce",
